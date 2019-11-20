c5229243eb20f447cc5dd224eb3f42b2c10ccfa4
v-wei40680 <48787443+v-wei40680@users.noreply.github.com>
2019-11-20 15:38:23 +0800

    trades group by year and month
    按年月统计销量

--- django-statistics.py
+++ django-statistics.py
@@ -7,11 +7,12 @@
 import datetime
 from dateutil.relativedelta import relativedelta
 
+from django.http import JsonResponse
 from django.db.models import Count
 from django.db.models.functions import (
   ExtractMonth, ExtractYear
 )
-from app.models import Data
+from app.models import Trade
 
 # 计算时间
 time_ago = datetime.datetime.now() - relativedelta(years=1)
@@ -24,3 +25,15 @@ count_res = one_year_data\
 print(count_res)
 
 url = "https://www.jb51.net/article/165680.htm"
+
+def trade_list(request, year, month):
+  trades = Trade.objects.filter(createTime__year=year, createTime__month=month)
+  month_res = trades.annotate(day=ExtractDay('createTime')).values('day').order_by('day')\
+      .annotate(count=Count('id'))
+  data = list(month_res)
+
+  return JsonResponse({'month_res': data})
+  
+urlpatterns = [
+  path('trades/<int:year>/<int:month>/', trade_list)
+]
